5fa3a582690f12e46d65ca4c393ede3df95d829f
Ahmed K Madani <ahmedkamal543@gmail.com>
2020-10-26 16:29:18 +0200

    change db con

--- dashboard/config.py
+++ dashboard/config.py
@@ -4,6 +4,6 @@ class Config():
     SECRET_KEY = 'farmula'
     socket_location = "/var/run/mysqld/mysqld.sock"
     # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/mizani"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:ahmed@12345@159.203.0.249/mizani"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:ahmed@12345@209.97.135.130/mizani"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
