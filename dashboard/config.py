41d63a5783692e71e84a81f4bd1f298dce89cb41
Ahmed K Madani <ahmedkamal543@gmail.com>
2020-11-04 13:12:09 +0200

    edit gitingore

--- dashboard/config.py
+++ dashboard/config.py
@@ -3,7 +3,7 @@ import os
 class Config():
     SECRET_KEY = 'farmula'
     socket_location = "/var/run/mysqld/mysqld.sock"
-    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/mizani"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:ahmed@12345@209.97.135.130/mizani"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/mizani"
+    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:ahmed@12345@209.97.135.130/mizani"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
