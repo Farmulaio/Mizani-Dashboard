70a12a167e18a87fb720b106d5e645bc5dcf8458
Ahmed K Madani <ahmedkamal543@gmail.com>
2020-11-13 14:34:06 +0200

    add module

--- dashboard/config.py
+++ dashboard/config.py
@@ -3,8 +3,8 @@ import os
 class Config():
     SECRET_KEY = 'farmula'
     socket_location = "/var/run/mysqld/mysqld.sock"
-    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/mizani"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/mizani"
     # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:ahmed@12345@209.97.135.130/mizani"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:ahmed@12345@3.15.217.143/mizani"
+    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:ahmed@12345@3.15.217.143/mizani"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
