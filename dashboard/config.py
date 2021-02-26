836aacd6bad29d245ccf34f9079aacef45f13a70
Ahmed K Madani <ahmedkamal543@gmail.com>
2021-02-26 19:44:30 +0200

    fix db con

--- dashboard/config.py
+++ dashboard/config.py
@@ -5,6 +5,6 @@ class Config():
     SECRET_KEY = 'farmula'
     socket_location = "/var/run/mysqld/mysqld.sock"
     # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/mizani"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:ahmed@209.97.135.130/mizani"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:ahmed@12345@209.97.135.130/mizani"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
